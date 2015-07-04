d87fd7dfffc3d72bf3b81192f55230cc4afad8b3
Ivan Nikitin <ivan@nikitin.biz>
2015-07-04 16:55:35 +0300

    v.0.1

---
+++ in-analytics.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: IN-Analytics
+Plugin URI: http://in-analytics.com/
+Description: Just another Google Analytics Plugin
+Version: 0.1
+Author: Ivan Nikitin
+Author URI: http://ivannikitin.com
+*/
+
